dfa2a1ec005f1b6e684d618ab641787d15eb0185
Prince Eklu <ameto888@protonmail.com>
2024-09-19 00:01:01 -0400

    Retrieve data in the Frontend.

--- src/Components/FetchData.jsx
+++ src/Components/FetchData.jsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import UseFetch from './UseFetch';
+import './FetchData.css'
 
 
 const FetchData = () => {
@@ -17,9 +18,34 @@ const FetchData = () => {
     <>
      <ul className='list_data_main'>
         <h1 className='usefetch_heading'>Use Fetch Custom Hook</h1>
+
+        {data && data.map(e=>(
+
+          <>
+
+          <li className='list_data'>
+
+          <h3>{e.name}</h3>
+
+          <p><strong>Importance: </strong>{e.importance}</p>
+
+          <p><strong>Benefits: </strong>{e.benefits6}</p>
+
+          <p><strong>Time to eat: </strong>{e.best_time_to_intake}</p>
+
+          </li>
+
+          </>
+
+        ))}
+
+
+
      </ul>
     </>
   )
 }
 
 export default FetchData
+
+
